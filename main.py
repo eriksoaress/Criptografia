7f366ddcde9bbe944bf6ee8813766e17b6d81f6e
eriksoaress <erikbtu2020@gmail.com>
2023-03-06 12:15:29 -0300

    enigma e deenigma em processo

--- main.py
+++ main.py
@@ -48,19 +48,50 @@ em uma mensagem recebida como entrada e retorna a mensagem cifrada. `P` é a
 matriz de permutação que realiza a cifra.'''
 def cifrar(msg, P):
     matriz_msg = para_one_hot(msg)
-    return P @ matriz_msg
+    result = P @ matriz_msg
+    return para_string(result)
 
 ''' Uma função `de_cifrar(msg : str, P : np.array)` que recupera uma mensagem
 cifrada, recebida como entrada, e retorna a mensagem original. `P` é a matriz de permutação que realiza a cifra.'''
 def de_cifrar(msg, P):
-    return np.linalg.inv(P) @ msg
+    msg = para_one_hot(msg)
+    result = np.linalg.inv(P) @ msg
+    return para_string(result)
 
 '''Uma função `enigma(msg : str, P : np.array, E : np.array)` que faz a cifra enigma na mensagem de entrada
 usando o cifrador `P` e o cifrador auxiliar `E`, ambos representados como matrizes de permutação.'''
-def enigma(msg, P, E):
-    pass
+# def enigma(msg, P, E):
+#     result1 = cifrar(msg, P)
+#     for indice in (result1.T):
+#         if indice != 0:
+#             for i in range(indice):
+#                 result1 = E @ result1
+#     return result1
 
 '''Uma função `de_enigma(msg : str, P : np.array, E : np.array)` que recupera uma mensagem cifrada como enigma
 assumindo que ela foi cifrada com o usando o cifrador `P` e o cifrador auxiliar `E`, ambos representados como matrizes de permutação.'''
-def de_enigma(msg, P, E):
-    pass
+# def de_enigma(msg, P, E):
+#     for indice in (msg.T):
+#         for i in range(indice):
+#             msg = np.linalg.inv(E)
+
+#         msg = np.linalg.inv(P) @ msg
+
+
+#     result2 = de_cifrar(result1, P)
+#     return result2
+
+
+matriz = np.zeros((26,26), dtype=int)
+matriz2 = np.zeros((26,26), dtype=int)
+
+
+for i in range(26):
+    matriz[i, (i+1)%26] = 1
+    matriz2[i, (i+2)%26] = 1
+
+teste_enigma = enigma('Olla a todos', matriz, matriz2)
+print(teste_enigma)
+
+teste_de_enigma = de_enigma(teste_enigma, matriz, matriz2)
+print(teste_de_enigma)
