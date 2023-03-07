725c9cade5a0065a86d33a44e8eff52d61c9a1b4
eriksoaress <erikbtu2020@gmail.com>
2023-03-07 19:03:38 -0300

    enigma e de_enigma funcionando

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-alfabeto =[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alfabeto =[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', "'", '!', '@','#','$','&','*','(',')','-','_','+','=', '-','.',',', '0','1','2','3','4','5','6','7','8','9','/']
 
 
 '''Uma função `para_one_hot(msg : str)` para codificar mensagens como uma matriz usando one-hot encoding'''
 def para_one_hot(msg):
-    matriz = np.zeros([26,len(msg)])
+    matriz = np.zeros([len(alfabeto),len(msg)])
     msg = msg.lower()
     for j, letra in enumerate(msg):
         if letra in alfabeto:
@@ -14,11 +14,11 @@ def para_one_hot(msg):
             matriz[i][j] = 1
         else:
             if letra.isspace():
-                for linha in range(26):
-                    matriz[linha][j] = 0
+                for coluna in range(len(alfabeto)):
+                    matriz[coluna][j] = 0
             else:
-                for linha in range(26):
-                    matriz[linha][j] = 1
+                for coluna in range(len(alfabeto)):
+                    matriz[coluna][j] = 1
     return matriz
 
 
@@ -27,9 +27,9 @@ def para_one_hot(msg):
 '''Uma função `para_string(M : np.array)` para converter mensagens da representação one-hot encoding para uma string legível'''
 def para_string(M):
     msg = ''
-    for linha in M.T:
+    for coluna in M.T:
         quantidade = 0
-        for i, elemento in enumerate(linha):
+        for i, elemento in enumerate(coluna):
             if elemento == 1:
                 quantidade+=1
                 indice = i
@@ -60,37 +60,46 @@ def de_cifrar(msg, P):
 
 '''Uma função `enigma(msg : str, P : np.array, E : np.array)` que faz a cifra enigma na mensagem de entrada
 usando o cifrador `P` e o cifrador auxiliar `E`, ambos representados como matrizes de permutação.'''
-# def enigma(msg, P, E):
-#     result1 = cifrar(msg, P)
-#     for indice in (result1.T):
-#         if indice != 0:
-#             for i in range(indice):
-#                 result1 = E @ result1
-#     return result1
+def enigma(msg, P, E):
+    matriz = ''
+    result1 = P@para_one_hot(msg)
+    for indice, coluna in enumerate(result1.T):
+        if indice != 0:
+            coluna = np.array([[x] for x in coluna])
+            for i in range(indice):
+                coluna = E @ coluna
+            matriz = np.hstack((matriz, coluna))
+        else:
+            matriz = np.array([[x] for x in coluna])
+    return para_string(matriz)
 
 '''Uma função `de_enigma(msg : str, P : np.array, E : np.array)` que recupera uma mensagem cifrada como enigma
 assumindo que ela foi cifrada com o usando o cifrador `P` e o cifrador auxiliar `E`, ambos representados como matrizes de permutação.'''
-# def de_enigma(msg, P, E):
-#     for indice in (msg.T):
-#         for i in range(indice):
-#             msg = np.linalg.inv(E)
+def de_enigma(msg, P, E):
+    matriz = ''
+    result1 = para_one_hot(msg)
+    for indice, coluna in enumerate(result1.T):
+        if indice != 0:
+            coluna = np.array([[x] for x in coluna])
+            for i in range(indice):
+                coluna = np.linalg.inv(E) @ coluna
+            coluna = np.linalg.inv(P) @ coluna
+            matriz = np.hstack((matriz, coluna))
+        else:
+            matriz = np.array([[x] for x in coluna])
+            matriz = np.linalg.inv(P) @ matriz
+    return para_string(matriz)
 
-#         msg = np.linalg.inv(P) @ msg
+
+matriz = np.zeros((len(alfabeto),len(alfabeto)), dtype=int)
+matriz2 = np.zeros((len(alfabeto),len(alfabeto)), dtype=int)
 
 
-#     result2 = de_cifrar(result1, P)
-#     return result2
+for i in range(len(alfabeto)):
+    matriz[i, (i+1)%len(alfabeto)] = 1
+    matriz2[i, (i+2)%len(alfabeto)] = 1
 
-
-matriz = np.zeros((26,26), dtype=int)
-matriz2 = np.zeros((26,26), dtype=int)
-
-
-for i in range(26):
-    matriz[i, (i+1)%26] = 1
-    matriz2[i, (i+2)%26] = 1
-
-teste_enigma = enigma('Olla a todos', matriz, matriz2)
+teste_enigma = enigma('aaaaaaaaaaaaaa    ?@:?  kkaa', matriz, matriz2)
 print(teste_enigma)
 
 teste_de_enigma = de_enigma(teste_enigma, matriz, matriz2)
