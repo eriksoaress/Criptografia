4ea889f060c9baebb04adcbaf80cfc0eb8256197
fernandovs4 <fervsantos3000@gmail.com>
2023-03-11 00:20:49 -0300

    adicionando comentarios

--- api/api.py
+++ api/api.py
@@ -6,15 +6,21 @@ app = Flask(__name__)
 # funcao que recebe uma mensagem e retorna a mensagem cifrada com enigma
 @app.route('/enigma', methods=['POST'])
 def enigma_mensagem():
+    # recebe a mensagem em formato json
     data = request.get_json()
+    # extrai a mensagem do json
     mensagem = data['mensagem']
+    # retorna a mensagem cifrada com enigma
     return jsonify({'mensagem_enigma': enigma(mensagem)})
 
 # funcao que recebe uma mensagem cifrada com enigma e retorna a mensagem original
 @app.route('/de-enigma', methods=['POST'])
 def de_enigma_mensagem():
+    # recebe a mensagem em formato json
     data = request.get_json()
+    # extrai a mensagem do json
     mensagem = data['mensagem']
+    # retorna a mensagem original
     return jsonify({'mensagem_de_enigma': de_enigma(mensagem)})
 
 
